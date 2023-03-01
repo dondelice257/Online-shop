707d94f7cb61e173a00cd1a93a3cb3124d705939
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-03-02 01:23:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-       var number = document.getElementById('number4');
+       var number = document.getElementById('number5');
         var plus = document.getElementById('plus');
         var mine = document.getElementById('mine');
         var reset = document.getElementById('reset');
